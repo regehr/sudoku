4c7d84faad45b1e2269f6069be5e5133b7e18c09
John Regehr <regehr@cs.utah.edu>
2019-01-21 22:28:57 -0700

    minimal build infrastructure

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: sud
+
+sud: sud.c
+	gcc -O sud.c -o sud
+
+test: sud
+	test.sh
---
+++ test.sh
@@ -0,0 +1,7 @@
+./sud ./sud1.txt | grep 'found 1 solutions' &&
+./sud ./sud2.txt | grep 'found 1 solutions' &&
+./sud ./sud3.txt | grep 'found 1 solutions' &&
+./sud ./sud4.txt | grep 'found 1 solutions' &&
+./sud ./sud6.txt | grep 'found 1 solutions' &&
+./sud ./sud7.txt | grep 'found 1 solutions'
+
